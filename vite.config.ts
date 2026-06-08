cb7d4840fdeb4dfd2f46c1a47d8ff7bc4ccfdd73
Sanuga <sanux2005@gmail.com>
2026-06-08 19:48:15 +0530

    V5

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/HR-New-Features/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [react()],
 })
